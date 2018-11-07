9ccf5df7a492abb46244da69aa1b72ea2458f799
Dmitriy <11lione1991@gmail.com>
2018-11-07 19:03:27 +0200

    add Task 2

--- iterableObject.js
+++ iterableObject.js
@@ -1,40 +1,65 @@
-function* generator() {
-  var value = 10
-  const map = new Map([
-    ['1', 'string'],
-    [7, 'numb'], 
-    [[1,2,3], 'array']
-  ])
+//Task2: 
+function sum(a, b) {
+	return a+b
+ }
+ 
+ function* asyncFlow() {
+	 const a = yield sum(5,5);
+	 const b = yield Promise.resolve(10);
+	 const c = yield 20;
+	 const d = yield a+b+c;
+	 console.log(d);
+ }
+ 
+ function run(gen) {
+	 const sum = gen().next().value + gen().next().value + gen().next().value + gen().next().value
+	 return console.log( sum )
+ }
+ 
+ run(asyncFlow)
+ 
+ // распечататься должно 40
 
-  map.set(1, 'num1')
 
-  // const newSet = new Set([
-  //   ['1', 'string'],
-  //   [7, 'numb'], 
-  //   [[1,2,3], 'array']
-  // ])
-  // newSet.add(['numb1', 1]).add(2)
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==--=-==-=-=--=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-  try {
-    yield value
-    yield value * 10 // при использовании
-    yield value - 6
-    yield* map
-    yield console.log( value * 30 )
-  } catch( err ) {
-    console.log( `Ошибка из метода throw() ${err}` )
-  }
+// function* generator() {
+//   var value = 10
+//   const map = new Map([
+//     ['1', 'string'],
+//     [7, 'numb'], 
+//     [[1,2,3], 'array']
+//   ])
 
-}
+//   map.set(1, 'num1')
 
-var obj = {
-  'key': 457
-}
-obj[Symbol.iterator] = generator // в метод Symbol.iterator мы передаём тело генератора
+//   // const newSet = new Set([
+//   //   ['1', 'string'],
+//   //   [7, 'numb'], 
+//   //   [[1,2,3], 'array']
+//   // ])
+//   // newSet.add(['numb1', 1]).add(2)
 
-for (let value of obj) {
-  console.log( value )
-}
+//   try {
+//     yield value
+//     yield value * 10 // при использовании
+//     yield value - 6
+//     yield* map
+//     yield console.log( value * 30 )
+//   } catch( err ) {
+//     console.log( `Ошибка из метода throw() ${err}` )
+//   }
+
+// }
+
+// var obj = {
+//   'key': 457
+// }
+// obj[Symbol.iterator] = generator // в метод Symbol.iterator мы передаём тело генератора
+
+// for (let value of obj) {
+//   console.log( value )
+// }
 
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=--=-=-=-=-=----=-==-=-=-=-
@@ -48,8 +73,10 @@ for (let value of obj) {
 // gen.throw( new Error( 'Error caught!' ) ) 
 // console.log(`gen()`,gen()) //нельзя вызывать. это обьект
 
+// -=-=-=-=-=--=-=-==-=-==-=-=-=-=-=-=--==-=-=-=-=-==-===--=-=-=-=-=-=---=-=-=-=-=-=-==-==
+
 // -=-===---=-=-=-=-=-=-=-=--=-==-==-==================------------------------------
-// Task:
+// Task1:
 // 	a = { 0:0, 1:1, 2:2 }
 // 	b = [...a]
 // 	result [0,1,2]
